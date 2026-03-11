6104dcb6557959331e311c0f0447622618fc80c3
rawebinx <ragraval55@gmail.com>
2026-03-11 14:22:54 +0530

    Create webinars.tsx

---
+++ src/pages/webinars.tsx
@@ -0,0 +1,8 @@
+export default function WebinarsPage() {
+  return (
+    <div style={{padding: "40px"}}>
+      <h1>Browse Webinars</h1>
+      <p>WebinX webinar discovery page is working.</p>
+    </div>
+  );
+}
