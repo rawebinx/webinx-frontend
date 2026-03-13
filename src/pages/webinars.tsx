88188ea2efb7b62ae7152043cf6d1ea5b96d9522
rawebinx <ragraval55@gmail.com>
2026-03-13 17:42:24 +0530

    Delete src directory
    remove duplicate src

--- src/pages/webinars.tsx
+++
@@ -1,8 +0,0 @@
-export default function WebinarsPage() {
-  return (
-    <div style={{padding: "40px"}}>
-      <h1>Browse Webinars</h1>
-      <p>WebinX webinar discovery page is working.</p>
-    </div>
-  );
-}
