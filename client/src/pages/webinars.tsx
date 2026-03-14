584dd7ef5ca3e1f5a0d5b492733244561c319471
rawebinx <ragraval55@gmail.com>
2026-03-14 14:32:31 +0530

    Update webinars.tsx

--- client/src/pages/webinars.tsx
+++ client/src/pages/webinars.tsx
@@ -195,7 +195,7 @@ export default function WebinarsPage() {
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
 
-            {trending.map((webinar: WebinarWithHost) => (
+            {trending.map((webinar: any) => (
               <WebinarCard
                 key={webinar.id}
                 webinar={webinar}
