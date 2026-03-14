1c99337036e94fc95d1c9112dda5f196c6aeb068
rawebinx <ragraval55@gmail.com>
2026-03-14 15:27:15 +0530

    Update api.ts

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,9 @@
 import { normalizeEvent } from "./normalizeEvent";
 
 export async function fetchEvents() {
-  const res = await fetch("https://webinx-backend.onrender.com/api/events");
+  const res = await fetch(
+    "https://webinx-backend.onrender.com/api/events"
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch events");
@@ -9,5 +11,11 @@ export async function fetchEvents() {
 
   const data = await res.json();
 
-  return data.map(normalizeEvent);
+  return data
+    .map(normalizeEvent)
+    .sort(
+      (a: any, b: any) =>
+        new Date(a.startTime).getTime() -
+        new Date(b.startTime).getTime()
+    );
 }
