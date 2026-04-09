a2e3f8ed2eb697747b4190c2f6a8c36fe390f49e
rawebinx <ragraval55@gmail.com>
2026-04-09 15:25:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Route, Switch } from "wouter";
+
 import Home from "./pages/home";
-import Browse from "./pages/browse";
-import Webinar from "./pages/webinar";
 import HostDetail from "./pages/host-detail";
+import About from "./pages/about";
+import Contact from "./pages/contact";
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/browse" component={Browse} />
-      <Route path="/webinar/:slug" component={Webinar} />
       <Route path="/hosts/:slug" component={HostDetail} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
     </Switch>
   );
 }
