21769e0e6c16119edad247cdccc2017866f135cf
rawebinx <ragraval55@gmail.com>
2026-04-11 12:05:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import Home from "./pages/home";
 import WebinarDetailPage from "./pages/webinar";
 import CategoryPage from "./pages/category";
+import SeoPage from "./pages/seo";
 
 function App() {
   const path = window.location.pathname;
 
   if (path.startsWith("/webinar/")) return <WebinarDetailPage />;
   if (path.startsWith("/category/")) return <CategoryPage />;
+
+  // ✅ PROGRAMMATIC SEO ROUTE
+  if (path.split("/").length === 2 && path !== "/") {
+    return <SeoPage />;
+  }
 
   return <Home />;
 }
