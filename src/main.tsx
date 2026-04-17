3cc4262240d0b5d9d919f550a135d6870914338b
rawebinx <ragraval55@gmail.com>
2026-04-17 14:02:56 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <HelmetProvider>
       <App />
-    </QueryClientProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
